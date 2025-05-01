30f5de09325bb9035e257df29f8d010565f4c378
projectDev <project.dev.team.git.1@gmail.com>
2025-05-01 09:00:47 +0700

    new project

---
+++ app/src/main/java/com/project/projectnew/productcatalog/MainActivity.java
@@ -0,0 +1,16 @@
+package com.project.projectnew.productcatalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.projectnew.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
