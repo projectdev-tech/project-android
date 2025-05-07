00c2e07ca24a6c5e63c3da7faa0e91f945354772
percivalyan <yoryan8888@gmail.com>
2025-05-07 16:34:30 +0700

    feat:push-notifications

---
+++ app/src/main/java/com/project/projectnew/ordercreation/TransaksiFragment.java
@@ -0,0 +1,50 @@
+package com.project.projectnew.ordercreation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.project.projectnew.R;
+
+public class TransaksiFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_transaksi, container, false);
+
+//        Button btnBuatNotifikasi = view.findViewById(R.id.btn_buat_notifikasi);
+//        btnBuatNotifikasi.setOnClickListener(v -> {
+//            Toast.makeText(getContext(), "Fitur buat notifikasi belum tersedia", Toast.LENGTH_SHORT).show();
+//        });
+
+        TextView textJudul1 = view.findViewById(R.id.textJudulTransaksi1);
+//        TextView textJudul2 = view.findViewById(R.id.textJudulTransaksi2);
+
+        if (textJudul1 != null) {
+            textJudul1.setOnClickListener(v -> {
+                Intent intent = new Intent(getActivity(), DetailTransaksiActivity.class);
+                startActivity(intent);
+            });
+        }
+
+//        if (textJudul2 != null) {
+//            textJudul2.setOnClickListener(v -> {
+//                Intent intent = new Intent(getActivity(), DetailActivityActivity.class);
+//                startActivity(intent);
+//            });
+//        }
+
+        return view;
+    }
+}
--- app/src/main/java/com/project/projectnew/productcatalog/MainActivity.java
+++ app/src/main/java/com/project/projectnew/productcatalog/MainActivity.java
@@ -1,10 +1,12 @@
 package com.project.projectnew.productcatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.project.projectnew.R;
+import com.project.projectnew.ordercreation.MenuNotifikasi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView notificationIcon = findViewById(R.id.notificationIcon);
+        notificationIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MenuNotifikasi.class);
+            startActivity(intent);
+        });
     }
 }
